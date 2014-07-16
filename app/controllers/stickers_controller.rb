e5a07e39cc580dc56d2bb6ba793f40aae2d04494
cbestiva <cbestiva@gmail.com>
2014-07-16 15:18:28 -0700

    added new and create method in stickers controller

---
+++ app/controllers/stickers_controller.rb
@@ -0,0 +1,20 @@
+class StickersController < ApplicationController
+  def new
+    @sticker = Sticker.new
+  end
+
+  def create
+    @sticker = Sticker.new(sticker_params)
+    if @sticker.save
+      redirect_to @sticker, notice: "Sticker was successfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def sticker_params
+    params.require(:sticker).permit(:name)
+  end
+end
